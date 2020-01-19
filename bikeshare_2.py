d5ada7ab97d4bd93e83bcf61f75499518e6355bb
Jannis Jehmlich <jjehmlich@yahoo.de>
2020-01-19 21:48:23 +0100

    docs: Explains dictionary usage

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -44,6 +44,7 @@ def get_filters():
     print("-"*40)
     print("Now that we have a city lets look at the months\n")
     month = ""
+    # dictionary to translate the numbers
     months = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 7:"July", 8:"August", 9:"September",10:"Oktober",11:"November", 12:"December"}
     print('Please type a number from 1 to 6 depending on which month you want to look at. There is sadly no data from July to December\n\
 If you want to look at all months enter "all"\n')
